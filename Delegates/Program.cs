473e0211c1065381052a54f1be140ebbc16d1408
Jesper Johnsen <jjohnsen@ebay.com>
2016-05-07 22:18:15 +0200

    Added projects for delegate and event

---
+++ Delegates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgressReporter p = WriteProgressToConsole;
+            p += WriteProgressToFile;
+            Util.HardWork(p);
+            Console.ReadLine();
+        }
+
+        static void WriteProgressToConsole(int percentComplete)
+        {
+            Console.WriteLine(percentComplete);
+        }
+
+        static void WriteProgressToFile(int percentComplete)
+        {
+            System.IO.File.WriteAllText("progress.txt", percentComplete.ToString());
+        }
+    }
+
+    public delegate void ProgressReporter(int percentComplete);
+
+    public class Util
+    {
+        public static void HardWork(ProgressReporter p)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                p(i*10);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new Stock("THPW");
+            stock.Price = 27.10M;
+
+            stock.PriceChanged += (sender, eventArgs) =>
+            {
+                if ((eventArgs.NewPrice - eventArgs.LastPrice)/eventArgs.LastPrice > 0.1M)
+                    Console.WriteLine("Alert, 10% stock price increase!");
+            };
+
+            stock.Price = 31.59M;
+            Console.ReadLine();
+        }
+
+        public class PriceChangedEventArgs : EventArgs
+        {
+            public readonly decimal LastPrice;
+            public readonly decimal NewPrice;
+
+            public PriceChangedEventArgs(decimal lastPrice, decimal newPrice)
+            {
+                LastPrice = lastPrice;
+                NewPrice = newPrice;
+            }
+        }
+
+        public class Stock
+        {
+            string symbol;
+            decimal price;
+
+            public Stock(string symbol)
+            {
+                this.symbol = symbol;
+            }
+
+            public event EventHandler<PriceChangedEventArgs> PriceChanged;
+
+            protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+            {
+                if (PriceChanged != null) PriceChanged(this, e);
+
+            }
+
+            public decimal Price
+            {
+                get { return price; }
+                set
+                {
+                    if (price == value) return;
+                    decimal oldPrice = price;
+                    price = value;
+                    OnPriceChanged(new PriceChangedEventArgs(oldPrice, price));
+                }
+            }
+        }
+    }
+}
