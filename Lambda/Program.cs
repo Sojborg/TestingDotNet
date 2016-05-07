efba0af9c4dcfc3ca9f4c299010cae4d2b10c786
Jesper Johnsen <jjohnsen@ebay.com>
2016-05-07 22:34:12 +0200

    added lambda project + example

---
+++ Lambda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> multipler = i => i * i;
+            var result = multipler(2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
